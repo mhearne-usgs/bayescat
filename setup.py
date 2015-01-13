27c6f5b0b1e2caafd3b481ad0cf7e2d30a1f4c05
Michael Hearne <mhearne@usgs.gov>
2015-01-12 19:12:47 -0700

    Added setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='bayescat',
+      version='0.1dev',
+      description='BayesLoc wrapper for catalog re-location',
+      author='Mike Hearne',
+      author_email='mhearne@usgs.gov',
+      url='',
+      packages=[],
+      scripts = ['bayescat.py'],
+)
